1dba264825184c4dfd6b7b6fb98f8d234aa6cfcf
pryrtmx <pryrtmx@738b547b-87e9-4bd5-bea0-d67ca5449274>
2019-07-29 16:40:13 +0000

    create the scintilla messages in a similar fashion

--- src/convert_sci_msgs.pl
+++ src/convert_sci_msgs.pl
@@ -1,14 +1,15 @@
 #!/usr/bin/env perl
 
-# using this to replace h2ph script,
-#   customized to convert into a hash in the resulting .pm, rather than the constant-subs that h2ph creates
+# derived from convert_npp_msgs.pl
+#   customized for scintilla, rather than Notepad++
+#   generates a %scimsg hash to contain the messages
 
 use warnings;
 use strict;
 use autodie;
 
-my $in_file = $ARGV[0] || './src/Notepad_plus_msgs.h';
-my $out_file = $ARGV[1] || './lib/Win32/Mechanize/NotepadPlusPlus/__npp_msgs.pm';
+my $in_file = $ARGV[0] || './src/Scintilla.h';
+my $out_file = $ARGV[1] || './lib/Win32/Mechanize/NotepadPlusPlus/__sci_msgs.pm';
 
 my $slurp = do {
     local $/;
@@ -16,7 +17,7 @@ my $slurp = do {
     <$fh>;
 };
 
-my %nppm = ( WM_USER => 0x400 );
+my %scimsg = ();
 
 $slurp =~ s/\\\n\s*/ /g;
 $slurp =~ s{//.*$}{}gim;
@@ -26,7 +27,7 @@ foreach ( $slurp =~ /^\h*enum\s\w+\s*{\s*.*?\s*}\s*;/gims ) {
     my ($list) = ($_ =~ /{\s*(.*?)\s*}/g);
     my @enums = split /,\s*/, $list;
     foreach my $i (0 .. $#enums) {
-        $nppm{ $enums[$i] } = $i;
+        $scimsg{ $enums[$i] } = $i;
     }
 }
 
@@ -35,27 +36,25 @@ foreach ( $slurp =~ /^\h*#\s*define\s+\w+.+$/gim ) {
     my ($name, $value) = ( m/^\h*#\s*define\s+(\w+)(.*)$/ );
     $value =~ s/^\s+//;
     $value =~ s/\s+$//;
-    $nppm{ $name } = $value;
+    $scimsg{ $name } = $value;
 }
-
-# TODO = need to replace
 
 open my $fh, '>', $out_file;
 print {$fh} "# auto-converted from $in_file at ", scalar localtime, "\n";
 print {$fh} <<'EOH';
-package Win32::Mechanize::NotepadPlusPlus::__npp_msgs;
+package Win32::Mechanize::NotepadPlusPlus::__sci_msgs;
 use warnings;
 use strict;
 use Exporter 5.57 ('import');
 
-our @EXPORT = qw/%nppm/;
-our %nppm = (
+our @EXPORT = qw/%scimsg/;
+our %scimsg = (
 EOH
 
 my %already;
-foreach my $key ( 'WM_USER', 'NPPMSG', 'RUNCOMMAND_USER', sort keys %nppm ) {
+foreach my $key ( sort keys %scimsg ) {
     next if $already{$key};
-    my $value = $nppm{$key};
+    my $value = $scimsg{$key};
     if( !defined $value ) {
         $value = 'undef';
     } elsif ($value eq '') {
@@ -64,17 +63,18 @@ foreach my $key ( 'WM_USER', 'NPPMSG', 'RUNCOMMAND_USER', sort keys %nppm ) {
         my @reps;
         while ( $value =~ m/([A-Z]\w+)/gi ) {
             my $rep = $1;
-            next if $rep eq 'nppm';
-            if( exists $nppm{$rep} ) {
+            next if $rep eq 'scimsg';
+            if( exists $scimsg{$rep} ) {
                 push @reps, $rep;
             }
         }
         foreach my $rep ( @reps ) {
-            $value =~ s/\b$rep\b/$nppm{$rep}/;
+            $value =~ s/\b$rep\b/$scimsg{$rep}/;
         }
     }
-    printf {$fh} "    %-40s => %s,\n", "'$key'", $value;
-    $nppm{$key} = $value;   # need the key to also update, so that recursive definitions are done properly
+    next if $value =~ /^Sci_\w+$/;
+    printf {$fh} "    %-60s => %s,\n", "'$key'", $value;
+    $scimsg{$key} = $value;   # need the key to also update, so that recursive definitions are done properly
     $already{$key} = 'done';
 }
 
