9f22d66df21c1956dc74f842419bd97eee81410a
PeterCJ <pryrtcode@pryrt.com>
2019-09-30 15:11:10 -0700

    do a menuNavigation recursion; I can find either MenuItemID for actions or MenuID for submenus; I can make a whole structure with the whole menu map... TODO = I should be able to take those lessons learned and create a find routine, which will take an array of level names (plaintext or accelerator-text) and try to find the right MenuItemID; if it doesn't find the first menu in the top-level menu, it will start one level down and try again

--- debug/menuNav.pl
+++ debug/menuNav.pl
@@ -7,15 +7,54 @@ use Data::Dumper; $Data::Dumper::Useqq=1;
 use strict;
 use warnings;
 
-my $menuHandle = notepad()->SendMessage( $nppm{NPPM_GETMENUHANDLE} , 1, 0);     printf "0x%016x\n", $menuHandle;
+my $menuHandle = notepad()->SendMessage( $nppm{NPPM_GETMENUHANDLE} , 1, 0);     #printf "0x%016x\n", $menuHandle;
 
 # GetMenu(): https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-getmenu
-my $menuID = GetMenu(notepad()->{_hwnd});  printf "menuID => 0x%016x\n", $menuID;
-my $count = GetMenuItemCount($menuID);      printf "menuItemCount => %d\n", $count;
+my $menuID = GetMenu(notepad()->{_hwnd});  #printf "menuID => 0x%016x\n", $menuID;
+my $count = GetMenuItemCount($menuID);     #printf "menuItemCount => %d\n", $count;
 for my $i ( 0 .. $count-1 ) {
     local $, = ", ";
-    print GetMenuItemInfo( $menuID, $i ), "\n";
+    #print GetMenuItemInfo( $menuID, $i ), "\n";
     local $, = "";
-    print "\t GetSubMenu = ", GetSubMenu($menuID, $i), "\n";
+    #print "\t GetSubMenu = ", GetSubMenu($menuID, $i), "\n";
+}
+print Dumper recurseMenu($menuID);
+exit;
+
+sub recurseMenu {
+    my $menuID = shift;
+    my $count = GetMenuItemCount( $menuID );
+    my @toRecurse;
+    my @entries;
+
+    # look through this whole menu first, gathering info
+    for my $idx ( 0 .. $count-1 ) {
+        my %h = GetMenuItemInfo( $menuID, $idx );
+        if( $h{type} eq 'separator') {
+            $entries[$idx] = "separator";
+        } elsif ( $h{type} eq 'string') {
+            $entries[$idx]{text} = $h{text};
+            ($entries[$idx]{plaintext} = $h{text}) =~ s/\&//;   # remove the hotkey indicator
+            $entries[$idx]{plaintext} =~ s/\t.*//;             # remove anything after TABs (which are usually the shortcut listing)
+            my $submenu = GetSubMenu($menuID, $idx);
+            if($submenu) {
+                $entries[$idx]{MenuID} = $submenu;
+                push @toRecurse, $idx;
+            } else {
+                $entries[$idx]{MenuItemID} = GetMenuItemID( $menuID, $idx );
+            }
+        } else {
+            $entries[$idx] = "!!type: $h{type}!!";
+        }
+    }
+
+    # now recurse into any that need it:
+    for my $idx ( @toRecurse ) {
+        $entries[$idx]{submenu} = recurseMenu( $entries[$idx]{MenuID} );
+    }
+
+    return [@entries];
 }
 
+__END__
+perl debug\menuNav.pl > recurse.pm && notepad++ recurse.pm
