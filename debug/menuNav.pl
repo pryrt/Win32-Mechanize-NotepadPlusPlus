f3380e72a78390c2af4d6eba1b28038e994ba930
PeterCJ <pryrtcode@pryrt.com>
2019-10-01 11:34:33 -0700

    in my debug script, able to search through menus and find the described action, whether given a hierarchical array, or a pipe-delimited string

--- debug/menuNav.pl
+++ debug/menuNav.pl
@@ -6,6 +6,7 @@ use Win32::GuiTest ':FUNC';
 use Data::Dumper; $Data::Dumper::Useqq=1;
 use strict;
 use warnings;
+use 5.012;
 
 my $menuHandle = notepad()->SendMessage( $nppm{NPPM_GETMENUHANDLE} , 1, 0);     #printf "0x%016x\n", $menuHandle;
 
@@ -18,8 +19,72 @@ for my $i ( 0 .. $count-1 ) {
     local $, = "";
     #print "\t GetSubMenu = ", GetSubMenu($menuID, $i), "\n";
 }
-print Dumper recurseMenu($menuID);
+#print Dumper recurseMenu($menuID);
+
+my $found = findActionInMenu($menuID, qw/Tools MD5 Generate.../) // '<undef>';
+print STDERR "ActionID = $found\n";
+
+$found = findActionInMenu($menuID, qw/MD5 Generate.../) // '<undef>';
+print STDERR "ActionID = $found\n";
+
+$found = findActionInMenu($menuID, qw/Plugins George/) // '<undef>';
+print STDERR "ActionID = $found\n";
+
+$found = findActionInMenu($menuID, 'Show Symbol | Show White Space and TAB') // '<undef>';
+print STDERR "ActionID = $found\n";
+
 exit;
+
+sub findActionInMenu {
+    my $menuID = shift;
+    my ($top, @hier) = @_;
+    my $count = GetMenuItemCount( $menuID );
+    state $topID = undef;
+    $topID = $menuID unless defined $topID;
+    state @recurse = ();                            # originally, was a my @recurse, so it went depth-first; by being state-with-shift, it holds its value in recursion, so it becomes a breadth-first
+
+    if($top =~ /\|/) {   # need to split into multiple levels
+        print STDERR "found PIPE '|'\n";
+        my @tmp = split /\|/, $top;
+        s/^\s+|\s+$//g for @tmp;     # trim spaces
+        $top = shift @tmp;          # top is really just the first element of the original top
+        unshift @hier, @tmp;        # prepend the @hier with the remaining elements of the split top
+        print STDERR "new (", join(', ', map { qq/'$_'/ } $top, @hier), ")\n";
+    }
+
+    for my $idx ( 0 .. $count-1 ) {
+        my %h = GetMenuItemInfo( $menuID, $idx );
+        if( $h{type} eq 'string' ) {
+            my $realText = $h{text};
+            (my $cleanText = $realText) =~ s/(\&|\t.*)//;
+            if( $top eq $realText or $top eq $cleanText ) {
+                #print STDERR "FOUND($top): $realText => $cleanText\n";
+                if( my $submenu = GetSubMenu($menuID, $idx) ) {
+                    return findActionInMenu( $submenu, @hier );
+                } elsif ( my $action = GetMenuItemID( $menuID, $idx ) ) {
+                    #print STDERR "\tthe action ID = $action\n";
+                    return $action;
+                } else {
+                    #print STDERR "\tcouldn't go deeper in the menu\n";
+                    return 0;
+                }
+            }
+        }
+        # this idx didn't match... but I may need it later (assuming it's a submenu)
+        if( my $submenu = GetSubMenu($menuID, $idx) ) {
+            push @recurse, $submenu;
+        }
+    }
+    #print STDERR "$menuID# ($top | @hier) wasn't found; try to recurse: (@recurse)\n";
+    if($menuID == $topID) { # only try recursion if we're at the top level
+        while( my $submenu = shift @recurse ) {
+            my $found = findActionInMenu( $submenu, $top, @hier );
+            return $found if $found;
+        }
+        #print STDERR "$menuID# ($top | @hier) wasn't found, even after recusion\n";
+    }
+    return undef;
+}
 
 sub recurseMenu {
     my $menuID = shift;
