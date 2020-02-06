ed7f777b9c59423cab88c1ea3f194d8ffeed309d
PeterCJ <pryrtcode@pryrt.com>
2020-02-06 06:37:15 -0800

    I can replicate the \0-end-of-text bug with strEndNull.pl; need to debug it

---
+++ debug/strEndNull.pl
@@ -0,0 +1,23 @@
+use warnings;
+use strict;
+use Data::Dumper; $Data::Dumper::Useqq=1;
+use Win32::Mechanize::NotepadPlusPlus qw/:main/;
+
+BEGIN {
+    notepad->newFile();
+    notepad->moveCurrentToOtherView();
+}
+
+END {
+    sleep(5);
+    editor->setText("");
+    notepad->close();
+}
+
+editor->setText("Hello World");
+my $got = editor->getText();
+
+use Test::More tests => 1;
+like $got, qr/^Hello World$/, 'No null at end'
+    or diag "\tgot ", Dumper $got;
+done_testing();
