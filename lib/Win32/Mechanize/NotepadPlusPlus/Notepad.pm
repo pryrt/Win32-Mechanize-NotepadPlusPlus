ad353051cce63dfe1e8f8bb4dbff56f8ea08b7b2
PeterCJ <pryrtcode@pryrt.com>
2020-06-04 13:12:41 -0700

    preparing for release; since 0.003001 contained new features, 0.003002 isn't a purely bugfix release, so update version to 0.004

--- lib/Win32/Mechanize/NotepadPlusPlus/Notepad.pm
+++ lib/Win32/Mechanize/NotepadPlusPlus/Notepad.pm
@@ -31,7 +31,7 @@ BEGIN {
     Win32::API::->Import("psapi","BOOL EnumProcessModules(HANDLE  hProcess, HMODULE *lphModule, DWORD   cb, LPDWORD lpcbNeeded)") or die "EnumProcessModules: $^E";  # uncoverable branch true
 }
 
-our $VERSION = '0.003002'; # auto-populated from W::M::NPP
+our $VERSION = '0.004'; # auto-populated from W::M::NPP
 
 our @EXPORT_VARS = (@Win32::Mechanize::NotepadPlusPlus::Notepad::Messages::EXPORT);
 our @EXPORT_OK = (@EXPORT_VARS);
